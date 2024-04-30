0b7f566795d6999cff8724f4bae214cc27b55951
uddhav.baniya <uddhav.baniya@midastechnologies.com.np>
2024-04-30 16:06:56 +0545

    writting

--- returnJSON.js
+++ returnJSON.js
@@ -5,13 +5,14 @@ dbConn.setAutoCommit(false);
 try {
     var labId = msg['PV1']['PV1.1']['PV1.1.1'].toString();
     var analyzerName = msg['MSH']['MSH.3']['MSH.3.1'].toString() + ' ' + msg['MSH']['MSH.4']['MSH.4.1'].toString();
-    var value = 'some val';
 
     for (var i = 0; i < obxCount; i++) {
         var identifier = msg['OBX'][i]['OBX.2']['OBX.2.1'].toString();
         if (identifier.indexOf("NM") > -1) {
-            var testCode = msg['OBX'][i]['OBX.3']['OBX.3.1'].toString();
-            var testDescription = msg['OBX'][i]['OBX.3']['OBX.3.2'].toString();
+            var testCode = msg['OBX'][i]['OBX.3']['OBX.3.2'].toString();
+            if (testCode.includes("%")) {
+                testCode = testCode.replace("%", "");
+            }
             var result = parseFloat(msg['OBX'][i]['OBX.5']['OBX.5.1'].toString());
             var units = msg['OBX'][i]['OBX.6']['OBX.6.1'].toString();
             var referenceRange = msg['OBX'][i]['OBX.7']['OBX.7.1'].toString();
@@ -34,25 +35,25 @@ try {
                 "FROM patient_test_details ptd " +
                 "LEFT JOIN tests t ON t.id = ptd.test_id " +
                 "LEFT JOIN patient_tests pt ON pt.id = ptd.patient_test_id " +
-                "WHERE t.abbreviation IS NOT NULL AND ptd.lab_id = '" + labId + "'" +
-                " AND t.abbreviation = '" + testCode + "'";
+                "WHERE t.abbreviation IS NOT NULL AND ptd.lab_id = '" + labId + "' " +
+                "AND t.abbreviation = '" + testCode + "'";
 
             var rs = dbConn.executeCachedQuery(sqlSelect);
             while (rs.next()) {
-                logger.info("Test ID: " + rs.getString("test_id") + ", Test: " + rs.getString("test") + ", Lab ID: " + rs.getString("lab_id"));
+                logger.info("YOUR ABBR IS : " + rs.getString("abbreviation"));
+                var sqlUpdate = "UPDATE patient_test_details SET finding = ?,analyzerresult = ?,analyzer_name = ? WHERE id = ? AND lab_id = ?";
+                var updateParams = new java.util.ArrayList();
+                updateParams.add(result);
+                updateParams.add(true);
+                updateParams.add(analyzerName);
+                updateParams.add(parseInt(rs.getString("id")));
+                updateParams.add(labId);
+                logger.info("Patient Test Detail Updated With Result : " + result);
+                dbConn.executeUpdate(sqlUpdate, updateParams);
             }
         }
     }
 
-    var sqlInsert = "INSERT INTO mirths (name, lab_id, value, gender) VALUES (?, ?, ?, ?)";
-    var params = new java.util.ArrayList();
-    params.add(analyzerName);
-    params.add(labId);
-    params.add(value);
-    params.add("Male");
-
-    dbConn.executeUpdate(sqlInsert, params);
-    logger.info("Data inserted successfully.");
     dbConn.commit();
 } catch (e) {
     logger.error("Error executing SQL query: " + e.message);
